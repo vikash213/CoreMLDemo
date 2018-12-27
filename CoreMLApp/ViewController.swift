b20268a976bcb41f77857a60676729d79f8788a5
Appinventiv <appinventiv@Apples-MacBook-Pro.local>
2018-12-27 15:07:31 +0530

    Initial Commit

---
+++ CoreMLApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLApp
+//
+//  Created by Appinventiv on 27/12/18.
+//  Copyright © 2018 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
